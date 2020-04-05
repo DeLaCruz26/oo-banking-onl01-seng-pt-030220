78d5c755801668c25c77d17596a427fc9b60334a
Dametrik Fick <dametrik.fick@gmail.com>
2020-04-05 04:56:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ class Transfer
 attr_reader :sender
 attr_accessor :transfer, :receiver
 
-  def initialize(transfer, receiver)
+  def initialize(transfer, sender, receiver)
     @transfer, @sender, @receiver = transfer, sender, receiver
   end
 end
