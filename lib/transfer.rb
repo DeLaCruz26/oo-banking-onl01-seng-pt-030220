f908b340b768ddc559bcab2e10158cd445ffdc42
Dametrik Fick <dametrik.fick@gmail.com>
2020-04-05 05:39:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,7 @@ attr_accessor :status
   end
 
   def reverse_transfer
-    if valid? && receiver.balance > amount && self.status == "pending"
+    if valid? && receiver.balance > amount && self.status == "complete"
       receiver.balance -= amount
       sender.balance += amount
       self.status = "complete"
