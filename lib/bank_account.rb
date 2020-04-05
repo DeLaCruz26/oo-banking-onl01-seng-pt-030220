df7795003e2f46eef4d4dcdb6ca5fcc3465dcb6f
Dametrik Fick <dametrik.fick@gmail.com>
2020-04-05 04:34:32 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,23 @@
 class BankAccount
 
+attr_reader :name
+attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(money)
+    money = @balance = 2000
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    valid? === true ? @status = "open" : balance > 0
+  end
 end
